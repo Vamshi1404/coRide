83a2cd5162990a21b4ce5b73067ed708e69ecf2f
Vamshi1404 <vamshithirumal787@gmail.com>
2026-08-23 16:50:31 +0530

    update1.1

--- frontend/src/lib/gsapSetup.js
+++ frontend/src/lib/gsapSetup.js
@@ -2,7 +2,7 @@ import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(useGSAP, ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger)
 
 gsap.defaults({ ease: 'expo.out', duration: 0.7 })
 
